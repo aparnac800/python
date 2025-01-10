6c093964dda76e6e7021ebd57b9ab42fff9c616c
aparnac800 <caparna800@gmail.com>
2025-01-10 11:58:45 +0530

    Delete list reoving.py

--- list reoving.py
+++
@@ -1,8 +0,0 @@
-l=[]
-n=int(input("enter a limit:"))
-for i in range(n):
-    v=int(input("enter values:"))
-    l.append(v)
-    print(l)
-    c=[n for x in l if x%2!=0]
-    print("list of even numbers:",c)
