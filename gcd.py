0a545c8ec6658ce1213febff546f0cd3e6382da7
aparnac800 <caparna800@gmail.com>
2025-01-10 11:58:29 +0530

    Delete gcd.py

--- gcd.py
+++
@@ -1,8 +0,0 @@
-def gcd(a,b):
-    if b==0:
-        return a
-    return gcd(b,a%b)
-a=int(input("enter the first number:"))
-b=int(input("enter the second number:"))
-result=gcd(a,b)
-print(f"the GCD of{a}and{b} is {result}")
